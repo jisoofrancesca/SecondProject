ed2b9beaab04f3b825b4b71cef4876b27bf4e796
jisoofrancesca <jisoofrancesca@gmail.com>
2016-07-20 15:16:27 +0900

    Hello.java modification on GitHub

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
+//For command line git tutorial
+//GitHub modification 
 public class Hello {
    public static void main (String [] args) {
        System.out.println ("Hello Git!") ;
-       System.out.println("What is Git?") ; 
-       System.out.println("That's DVCS!") ;
-       System.out.println("2002, BitKeeper")
+       System.out.println ("What is Git?") ; 
+       System.out.println ("That's DVCS!") ;
+       System.out.println ("2002, BitKeeper")
    }
 }
